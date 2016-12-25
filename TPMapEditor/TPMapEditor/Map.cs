ac7475da1a44d52eb191de1b70ca215252188c53
Paul Maxim <paulmaximgames@gmail.com>
2016-12-25 22:34:22 +0200

    Added the tiles and decided to store the absolute path of the map. Added Save and Load functionalities.

--- TPMapEditor/TPMapEditor/Map.cs
+++ TPMapEditor/TPMapEditor/Map.cs
@@ -1,19 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace TPMapEditor
 {
-    class Map
-    {
-        short tileSize;
-        Size gridSize;
-        string title;
-        string background;
-        string tileset;
+	class Map
+	{
+		short tileSize;
+		string path;
+		string background;
+		string tileset;
+		Size gridSize;
+		public Tile[,] Tiles { get; set; }
 
         /// <summary>
         /// Constructs a map with a basic structure.
@@ -21,40 +20,121 @@ namespace TPMapEditor
         public Map()
         {
             TileSize = 50;
-            GridSize = new Size(10, 10);
-            Title = "Unnamed";
-            Background = "Unnamed";
+            GridSize = new Size(1, 1);
+            FilePath = string.Empty;
+			Background = "Unnamed";
             Tileset = "Unnamed";
-        }
+            Tiles = new Tile[GridSize.Height, GridSize.Width];
 
+			for (int row = 0; row < GridSize.Height; ++row)
+			{
+				for (int column = 0; column < GridSize.Width; ++column)
+				{
+					Tiles[row, column] = new Tile();
+				}
+			}
+		}
+        
         /// <summary>
-        /// Constructs a map with a basic tilesize and gridsize but with specific title, background and tileset.
+        /// Loads the map by using the absolute path of the map file.
         /// </summary>
-        public Map(string Title, string Background, string Tileset)
+        /// <param name="absolutePath">The absolute path of the map file.</param>
+        public void Load(string absolutePath)
         {
-            TileSize = 50;
-            GridSize = new Size(10, 10);
-            this.Title = Title;
-            this.Background = Background;
-            this.Tileset = Tileset;
+			string FilePath = absolutePath;
+
+			string[] fileLines = File.ReadAllLines(FilePath);
+			int totalLines = fileLines.Length;
+
+			Size GridSize = new Size(Convert.ToInt32(fileLines[FileLineWithProperty.GridSize].Split(' ')[1]), 
+								Convert.ToInt32(fileLines[FileLineWithProperty.GridSize].Split(' ')[0]));
+
+			if (totalLines != GridSize.Height + FileLineWithProperty.Total)
+				throw new ArgumentOutOfRangeException("The map has more or less rows of tiles (" + (totalLines - FileLineWithProperty.Total) +
+													  ") than it's initially specified height (" + GridSize.Height + ").");
+
+			Tile[,] Tiles = new Tile[GridSize.Height, GridSize.Width];
+
+			string Tileset = fileLines[FileLineWithProperty.Tileset];
+			string Background = fileLines[FileLineWithProperty.Background];
+			short TileSize = Convert.ToInt16(fileLines[FileLineWithProperty.TileSize]);
+
+			for (int row = 0; row < GridSize.Height; ++row)
+			{
+				int currentLine = row + FileLineWithProperty.Total;
+				string[] tilesToBeParsed = fileLines[currentLine].Split(new []{ ' ', ',', ';', '(', ')', '[', ']', '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (GridSize.Width * Tile.TotalProperties != tilesToBeParsed.Length)
+					throw new ArgumentOutOfRangeException("The map has more or less tiles per row (" + (tilesToBeParsed.Length / Tile.TotalProperties) +
+														  ") than it's initially specified row's width (" + GridSize.Width + "), " +
+														  "either it has invalid separators used in delimiting the tiles.");
+
+				for (int column = 0; column < GridSize.Width; ++column)
+				{
+					int firstPropertyColumn = column * Tile.TotalProperties;
+
+					Tiles[row, column] = new Tile();
+					Tiles[row, column].PropertiesID = Convert.ToByte(tilesToBeParsed[firstPropertyColumn]);
+					Tiles[row, column].Damage = Convert.ToInt16(tilesToBeParsed[firstPropertyColumn + 1]);
+					Tiles[row, column].Solidity = (Solidity)Convert.ToInt32(tilesToBeParsed[firstPropertyColumn + 2]);
+				}
+			}
+
+			this.FilePath = FilePath;
+			this.GridSize = GridSize;
+			this.Tiles = Tiles;
+			this.Tileset = Tileset;
+			this.Background = Background;
+			this.TileSize = TileSize;
+		}
+
+		public void Save()
+		{
+			using (StreamWriter writer = new StreamWriter(FilePath))
+			{
+				writer.WriteLine(string.Join(" ", GridSize.Height, GridSize.Width));
+				writer.WriteLine(Tileset);
+				writer.WriteLine(Background);
+				writer.WriteLine(TileSize);
+
+				for(int row = 0; row < GridSize.Height; ++row)
+				{
+					for(int column = 0; column < GridSize.Width; ++column)
+					{
+						writer.Write(Tiles[row, column]);
+					}
+					writer.Write(Environment.NewLine);
+				}
+			}
+		}
+
+		struct FileLineWithProperty
+		{
+			public const int GridSize = 0;
+			public const int Tileset = 1;
+			public const int Background = 2;
+			public const int TileSize = 3;
+			public const int Total = 4;
+		};
+
+		/// <summary>
+		/// Gets the title (name) of the map.
+		/// </summary>
+		/// <returns>Returns a string that contains the map's title (name).</returns>
+		public string Title
+        {
+            get { return Path.GetFileNameWithoutExtension(path); }
         }
 
-        /// <summary>
-        /// Gets the title (name) of the map.
-        /// </summary>
-        /// <returns>Returns a string that contains the map's title (name).</returns>
-        public string Title
-        {
-            get { return title; }
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentNullException();
-
-                title = value;
-                OnTitleChanged(EventArgs.Empty);
-            }
-        }
+		public string FilePath
+		{
+			get { return path; }
+			set
+			{
+				path = value;
+				OnTitleChanged(EventArgs.Empty);
+			}
+		}
    
         /// <summary>
         /// Gets the size of a tile in pixels. All the tiles of the map will always have the same size.
@@ -65,11 +145,11 @@ namespace TPMapEditor
             get { return tileSize; }
             set
             {
-                if (value > 0)
-                {
-                    tileSize = value;
-                    OnTileSizeChanged(EventArgs.Empty);
-                }
+				if (value <= 0)
+					throw new ArgumentException("Invalid tile size.");
+
+				tileSize = value;
+				OnTileSizeChanged(EventArgs.Empty);
             }
         }
 
@@ -82,11 +162,11 @@ namespace TPMapEditor
             get { return gridSize; }
             set
             {
-                if (value.Width > 0 && value.Height > 0)
-                {
-                    gridSize = value;
-                    OnGridSizeChanged(EventArgs.Empty);
-                }
+				if (value.Width <= 0 || value.Height <= 0)
+					throw new ArgumentException("Invalid map size.");
+
+				gridSize = value;
+				OnGridSizeChanged(EventArgs.Empty);
             }
         }
 
@@ -112,7 +192,7 @@ namespace TPMapEditor
             set
             {
                 if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentNullException();
+                    throw new ArgumentNullException("Background's string is null.");
 
                 background = value;
                 OnBackgroundChanged(EventArgs.Empty);
@@ -129,7 +209,7 @@ namespace TPMapEditor
             set
             {
                 if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentNullException();
+                    throw new ArgumentNullException("Tileset's string is null.");
 
                 tileset = value;
                 OnTilesetChanged(EventArgs.Empty);
