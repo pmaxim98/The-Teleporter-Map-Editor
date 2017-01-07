350ca0cebbfc2075b28312368bb560d5cbc4d069
Paul Maxim <paulmaximgames@gmail.com>
2017-01-07 15:51:08 +0200

    Finished the code for the whole form.

--- The Teleporter/MapEditor.Forms/NewMapForm.Designer.cs
+++ The Teleporter/MapEditor.Forms/NewMapForm.Designer.cs
@@ -28,17 +28,40 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewMapForm));
+			this.NewMapElementHost = new System.Windows.Forms.Integration.ElementHost();
+			this.newMapControl = new MapEditor.WPF.NewMapControl();
 			this.SuspendLayout();
+			// 
+			// NewMapElementHost
+			// 
+			this.NewMapElementHost.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.NewMapElementHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.NewMapElementHost.Location = new System.Drawing.Point(0, 0);
+			this.NewMapElementHost.Name = "NewMapElementHost";
+			this.NewMapElementHost.Size = new System.Drawing.Size(320, 200);
+			this.NewMapElementHost.TabIndex = 0;
+			this.NewMapElementHost.Text = "NewMapElementHost";
+			this.NewMapElementHost.Child = this.newMapControl;
 			// 
 			// NewMapForm
 			// 
-			this.ClientSize = new System.Drawing.Size(334, 211);
+			this.ClientSize = new System.Drawing.Size(320, 200);
+			this.Controls.Add(this.NewMapElementHost);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "NewMapForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Create a new map";
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Integration.ElementHost NewMapElementHost;
+		private WPF.NewMapControl newMapControl;
 	}
 }
--- The Teleporter/MapEditor.Forms/NewMapForm.cs
+++ The Teleporter/MapEditor.Forms/NewMapForm.cs
@@ -1,18 +1,54 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using System.Drawing;
 
 namespace MapEditor.Forms
 {
 	public partial class NewMapForm : Form
 	{
+		public DialogResult Response { get; private set; }
+		public MapData MapData { get; private set; }
+
 		public NewMapForm()
 		{
 			InitializeComponent();
 
-//			Control[] controls = NewMapElementHost.Controls.Find("CreateButton", false);
+			Response = DialogResult.Cancel;
 
-	//		foreach (var ctrl in controls)
-		//		MessageBox.Show(ctrl.ToString());
+			newMapControl.CancelButtonClick += (s, e) =>
+			{
+				Response = DialogResult.Cancel;
+				Close();
+			};
+
+			newMapControl.CreateButtonClick += (s, e) =>
+			{
+				MapData = new MapData(
+					((System.Windows.Controls.TextBox)newMapControl.FindName("WidthBox")).Text,
+					((System.Windows.Controls.TextBox)newMapControl.FindName("HeightBox")).Text,
+					((System.Windows.Controls.ComboBox)newMapControl.FindName("BackgroundComboBox")).Text,
+					((System.Windows.Controls.ComboBox)newMapControl.FindName("TilesetComboBox")).Text,
+					((System.Windows.Controls.TextBox)newMapControl.FindName("TileSizeBox")).Text
+				);
+
+				Response = DialogResult.OK;
+				Close();
+			};
+		}
+	}
+
+	public class MapData
+	{
+		public string Background { get; private set; }
+		public Size GridSize { get; private set; }
+		public string Tileset { get; private set; }
+		public short TileSize { get; private set; }
+
+		public MapData(string width, string height, string background, string tileset, string tilesize)
+		{
+			Background = background;
+			GridSize = new Size(int.Parse(width), int.Parse(height));
+			Tileset = tileset;
+			TileSize = short.Parse(tilesize);
 		}
 	}
 }
