be0a12c9f3bb9a4f46f31f359826f71ae7c78bab
Paul Maxim <paulmaximgames@gmail.com>
2017-01-07 15:53:09 +0200

    Added functions for getting the default tilesets and backgrounds folders. Added a string to numeric function.

--- The Teleporter/MapEditor.Utilities/Utils.cs
+++ The Teleporter/MapEditor.Utilities/Utils.cs
@@ -10,5 +10,32 @@ namespace MapEditor.Utilities
 			Directory.CreateDirectory(mapsFolderPath);
 			return mapsFolderPath;
 		}
+
+		public static string GetDefaultTilesetsFolderPath()
+		{
+			string tilesetsFolderPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "Graphics\\Tilesets");
+			Directory.CreateDirectory(tilesetsFolderPath);
+			return tilesetsFolderPath;
+		}
+
+		public static string GetDefaultBackgroundsFolderPath()
+		{
+			string backgroundsFolderPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "Graphics\\Backgrounds");
+			Directory.CreateDirectory(backgroundsFolderPath);
+			return backgroundsFolderPath;
+		}
+
+		public static string FormatToNumericString(string normalString)
+		{
+			string formattedText = string.Empty;
+
+			foreach (char ch in normalString)
+			{
+				if (char.IsDigit(ch))
+					formattedText += ch;
+			}
+
+			return (formattedText == string.Empty) || formattedText == "0" ? string.Empty : short.Parse(formattedText).ToString();
+		}
 	}
 }
